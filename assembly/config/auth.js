29caaa89046bdeda7e8ae540b400bdd8cbd3971c
Ana Getovska <ani.evgenieva17@gmail.com>
2020-11-20 17:07:58 +0200

    fixed Admin bug

--- assembly/config/auth.js
+++ assembly/config/auth.js
@@ -2,18 +2,16 @@ exports.isUser = function (req, res, next) {
     if (req.isAuthenticated()) {
         next();
     } else {
-        req.flash('danger', 'Please log in');
-        res.redirect('/users/login');
+        res.redirect('/);
     }
 
 }
 
 exports.isAdmin = function (req, res, next) {
-    if (req.isAuthenticated()) {
+    if (req.isAuthenticated() && req.user.admin === true) {
         next();
     } else {
-        req.flash('danger', 'Please log in as Admin');
-        res.redirect('/users/login');
+        res.redirect('/');
     }
 
 }
